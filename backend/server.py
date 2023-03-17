fdece2d29190e639fd5e5a04935135c612a2f1f6
shjchou92 <shjchou92@gmail.com>
2023-03-17 00:09:03 -0400

    Add demo

--- backend/server.py
+++ backend/server.py
@@ -2,11 +2,10 @@ from flask import Flask, jsonify
 from flask import request, g
 from flask_cors import CORS, cross_origin
 
-from gamestop_scrapper import fetch_price
+from .gamestop_scrapper import fetch_price
 
 app = Flask(__name__)
 CORS(app)
-ENV = 'prod'
 
 @app.route('/gamestop', methods=['GET'])
 @cross_origin()
@@ -15,10 +14,3 @@ def get_info():
     info = fetch_price(url)
     info_final = jsonify(info)
     return info_final
-
-# http://127.0.0.1:5000/test
-
-@app.route('/test', methods=['GET'])
-def hello():
-    print('hello world')
-    return 'hello world'
